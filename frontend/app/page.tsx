eb7aff5846b9cfb0b6f142ba3bf166789e62c892
João Zenaro <joaomarcelo661@gmail.com>
2025-10-25 04:59:16 +0000

    feat: Complete supabase auth on frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getClaims();
+
+  if (error || !data?.claims) {
+    redirect("/auth/login");
+  } else {
+    redirect("/protected");
+  }
+}
